9f3ae29fbbae731a05490e33843459547504cad2
lookininward <vinoth.michaelxavier@gmail.com>
2018-08-25 12:45:54 -0400

    Update the book route template with delete actions and prompt, update the book route to reset properties on controller on load, delete action to persist deletion of book record

--- client/app/routes/book.js
+++ client/app/routes/book.js
@@ -6,5 +6,20 @@ export default Route.extend({
 
   model(book) {
     return this.get('store').peekRecord('book', book.book_id);
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+    this.controller.set('confirmingDelete', false);
+  },
+
+  actions: {
+
+    delete(book) {
+      book.deleteRecord();
+      book.save().then(() => {
+        this.transitionTo('books');
+      });
+    }
   }
 });
